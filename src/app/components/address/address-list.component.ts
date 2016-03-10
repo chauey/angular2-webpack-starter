94d9aa028d044817340daa0a3a7f95ea320bf28e
Duc Ho <duc.ho@dnafor.net>
2016-03-10 17:53:38 +0700

    - Added new pipes transform data for filtering display data.

--- src/app/components/address/address-list.component.ts
+++ src/app/components/address/address-list.component.ts
@@ -3,22 +3,24 @@ import {RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import { DataService } from '../../services/DataService';
 
+import { DataTransformPipe } from '../../pipes/dataTransform.pipe';
+
 import { Address } from '../../../API/Client/Address';
 
 import { AddressesApiLocal } from '../../../API/Client/AddressesApiLocal';
 import { IAddressesApi } from '../../../API/Client/IAddressesApi';
+import { StateProvinceApiLocal } from '../../../API/Client/StateProvinceApiLocal';
 
 //import {Pagination} from 'ng2-bootstrap/ng2-bootstrap';
 //import ng2Bootstrap = require('ng2-bootstrap/');
 //import {Pagination} from 'ng2-bootstrap/components/pagination';
 import { PAGINATION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
-
 @Component({
   selector: 'address-list',
   directives: [...ROUTER_DIRECTIVES, ...PAGINATION_DIRECTIVES ],
-  pipes: [],
-  //providers: [AddressesApiLocal], // AddressesApi],
+  pipes: [DataTransformPipe],
+  providers: [StateProvinceApiLocal], // AddressesApi],
   styles: [require('./address-list.component.css')],
   template: require('./address-list.component.html')
 })
@@ -28,6 +30,7 @@ export class AddressListComponent implements OnInit {
   searchValue: string = '';
   errorMessage: string;
   listWithCount: any;
+  stateProvinceList: any;
 
   top: number = 5;
   skip: number = 0;
@@ -45,8 +48,11 @@ export class AddressListComponent implements OnInit {
     // itemsPerPage: this.top
   };
 
-  constructor(private _router: Router, private _dataService: DataService,
-    private _AddressesApiLocal: AddressesApiLocal // AddressesApi
+  constructor(
+    private _router: Router,
+    private _dataService: DataService,
+    private _AddressesApiLocal: AddressesApiLocal,
+    private _StateProvinceApiLocal: StateProvinceApiLocal // AddressesApi
   ) {
   }
 
@@ -57,6 +63,12 @@ export class AddressListComponent implements OnInit {
   }
 
   getList() {
+    this._StateProvinceApiLocal
+      .get()
+      .subscribe(
+        (stateListWithCount) => { this.stateProvinceList = stateListWithCount.list; },
+        (error) => { this.errorMessage = <any>error; });
+
     this._AddressesApiLocal.addressesGet(this.expand, this.filter, this.select, this.orderBy, this.top, this.skip, this.count, null)
       .subscribe(
       addressListWithCount => this.listWithCount = addressListWithCount,
